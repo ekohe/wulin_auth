71b18720bdc915abe764567da7f700bf2eff22ce
Jimmy Huang <pake007@gmail.com>
2011-09-16 14:14:21 +0800

    user has many grid states

--- app/models/wulin_auth/user.rb
+++ app/models/wulin_auth/user.rb
@@ -2,5 +2,7 @@ class WulinAuth::User < ActiveRecord::Base
   attr_accessible :login, :password, :password_confirmation
   has_secure_password
   validates :login, :presence => true, :uniqueness => true
+  
+  has_many :grid_states
 end
 
