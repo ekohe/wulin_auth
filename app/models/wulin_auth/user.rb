09683643d292055e025585759e7f179e4bbcc07e
Xuhao <xuhao@rubyfans.com>
2011-09-16 16:22:58 +0800

    Move association to wulin_master

--- app/models/wulin_auth/user.rb
+++ app/models/wulin_auth/user.rb
@@ -2,7 +2,5 @@ class WulinAuth::User < ActiveRecord::Base
   attr_accessible :login, :password, :password_confirmation
   has_secure_password
   validates :login, :presence => true, :uniqueness => true
-  
-  has_many :grid_states
 end
 
