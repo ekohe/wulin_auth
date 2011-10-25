1f0e756912b88c5ea01957f39d07390915e87fd9
Maxime Guilbot <maxime@ekohe.com>
2011-10-25 18:54:21 +0800

    Setup wulin auth for assets compilation

--- lib/engine.rb
+++ lib/engine.rb
@@ -5,5 +5,8 @@ require 'application_helper'
 
 module WulinAuth
   class Engine < ::Rails::Engine
+    initializer "add assets to precompile" do |app|
+       app.config.assets.precompile += %w( login.js login.css )
+    end
   end
 end
