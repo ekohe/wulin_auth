f8cbfe78f3d79f3821a2bf82a87df580835c94f5
Maxime Guilbot <maxime@ekohe.com>
2012-02-27 12:13:20 +0800

    MISC - Update for Rails 3.2.1 & add current_user to abstract controller so that it's recognized in Metal

--- lib/application_controller.rb
+++ lib/application_controller.rb
@@ -8,40 +8,40 @@ module WulinAuth
         helper_method :current_user
       end
     end
+
+    # Instance Methods
       
-    module InstanceMethods
-      def current_user
-        @current_user ||= WulinAuth::User.find(session[:user_id]) if session[:user_id]
-      end
+    def current_user
+      @current_user ||= WulinAuth::User.find(session[:user_id]) if session[:user_id]
+    end
 
-      def on_login_page?
-        params[:controller] == "wulin_auth/user_sessions" && ["new", "create"].include?(params[:action])
-      end
+    def on_login_page?
+      params[:controller] == "wulin_auth/user_sessions" && ["new", "create"].include?(params[:action])
+    end
 
-      def require_login
-        if current_user.nil?
-          return unauthorized_response
-        else
-          session.delete(:return_to) if session[:return_to]
-          logger.warn "Authenticated as #{current_user.email}"
+    def require_login
+      if current_user.nil?
+        return unauthorized_response
+      else
+        session.delete(:return_to) if session[:return_to]
+        logger.warn "Authenticated as #{current_user.email}"
+      end
+    end
+
+    def unauthorized_response(message="You must be logged in to access this page.")
+      respond_to do |format|
+        format.html do
+          session[:return_to] = request.get? ? request.url : nil
+          Rails.logger.info "Authentication needed, redirecting to login page"
+          Rails.logger.info "Saving return to url to #{session[:return_to]}" if session[:return_to]
+          flash[:notice] = message
+          redirect_to login_path
         end
+        format.json { render :json => {:error => :not_authorized} }
       end
-
-      def unauthorized_response(message="You must be logged in to access this page.")
-        respond_to do |format|
-          format.html do
-            session[:return_to] = request.get? ? request.url : nil
-            Rails.logger.info "Authentication needed, redirecting to login page"
-            Rails.logger.info "Saving return to url to #{session[:return_to]}" if session[:return_to]
-            flash[:notice] = message
-            redirect_to login_path
-          end
-          format.json { render :json => {:error => :not_authorized} }
-        end
-        false
-      end
+      false
     end
   end
 end
 
-::ActionController::Base.send :include, WulinAuth::Controller
+::AbstractController::Base.send :include, WulinAuth::Controller
