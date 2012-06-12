b2afb6556ebf222192115d122e3aed6aa0805a97
Xuhao <xuhao@rubyfans.com>
2012-06-12 12:19:49 +0800

    MISC - Add presence validate for password

--- app/models/wulin_auth/user.rb
+++ app/models/wulin_auth/user.rb
@@ -2,5 +2,6 @@ class WulinAuth::User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
   has_secure_password
   validates :email, :presence => true, :uniqueness => true
+  validates :password, :presence => true
 end
 
