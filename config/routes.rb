8670fe884bf80400516ac719bf5a13c5afdbd01c
Xuhao <xuhao@rubyfans.com>
2012-06-14 16:48:24 +0800

    MISC - Add remote_logout action for logout form other app

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   match 'login' => "wulin_auth/user_sessions#new", :as => :login, :via => :get
   match 'login' => "wulin_auth/user_sessions#create", :as => :login, :via => :post
   match 'logout' => "wulin_auth/user_sessions#destroy", :as => :logout
+  match 'remote_logout' => "wulin_auth/user_sessions#remote_destroy", :as => :remote_logout
 end
