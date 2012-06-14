4fe02abcafc0f015d4a93f27b5c93eb3897a6677
Xuhao <xuhao@rubyfans.com>
2012-06-14 17:53:59 +0800

    MISC - Remove 'remote_destroy' action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   match 'login' => "wulin_auth/user_sessions#new", :as => :login, :via => :get
   match 'login' => "wulin_auth/user_sessions#create", :as => :login, :via => :post
   match 'logout' => "wulin_auth/user_sessions#destroy", :as => :logout
-  match 'remote_logout' => "wulin_auth/user_sessions#remote_destroy", :as => :remote_logout
 end
