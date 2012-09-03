9f33fb97ae221b1a0632a1c29fc534716c644fc6
Xuhao <xuhao@rubyfans.com>
2012-09-03 12:17:11 +0800

    MISC - Add missing attr_accessible attribute

--- app/models/wulin_auth/user.rb
+++ app/models/wulin_auth/user.rb
@@ -1,7 +1,5 @@
 class WulinAuth::User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :user_level
   has_secure_password
   validates :email, :presence => true, :uniqueness => true
-  validates :password, :presence => true
 end
-
