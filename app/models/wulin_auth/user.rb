ef9100e53ef475d5a290132a7579301d40368863
Xuhao <xuhao@rubyfans.com>
2013-01-15 16:04:15 +0800

    MISC - Remove protected attributes in user model

--- app/models/wulin_auth/user.rb
+++ app/models/wulin_auth/user.rb
@@ -1,5 +1,5 @@
 class WulinAuth::User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation, :user_level
+  # attr_accessible :email, :password, :password_confirmation, :user_level
   has_secure_password
   validates :email, :presence => true, :uniqueness => true
 end
