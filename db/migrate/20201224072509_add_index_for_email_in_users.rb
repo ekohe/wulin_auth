014c719536d1675a7c56db7126afa63ec2924ca0
Maxime Guilbot <maxime@ekohe.com>
2021-01-11 13:44:30 +0900

    #7 - keep compatibility with Rails 5

--- db/migrate/20201224072509_add_index_for_email_in_users.rb
+++ db/migrate/20201224072509_add_index_for_email_in_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddIndexForEmailInUsers < ActiveRecord::Migration[6.0]
+class AddIndexForEmailInUsers < ActiveRecord::Migration[5.1]
   unless index_exists?(:users, :email, unique: true)
     def change
       add_index(:users, :email, unique: true)
